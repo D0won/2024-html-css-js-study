fa007509a5208fab6eb8fabb1708998dc529ff97
Dowon Lee <ldwlee03@gmail.com>
2024-05-31 19:42:59 +0900

    n

--- 웹 프로그래밍 수업/week13/binary.js
+++ 웹 프로그래밍 수업/week13/binary.js
@@ -5,7 +5,7 @@ function ConvertToBinary() {
     
     var binary1 = parseInt(input1, 10).toString(2);
     var binary2 = parseInt(input2, 10).toString(2);
-
+    
     document.getElementById('binary').innerHTML = "입력1의 2진수: " + binary1 + "<br>입력2의 2진수: " + binary2;
    
     
